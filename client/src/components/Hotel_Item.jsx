c3b59c2ed0a4e6bd19496446d4e2fa2ed1ebdfc8
SairaMadrid <sairakbar@gmail.com>
2023-05-28 09:56:57 +0200

    React router dom installed

--- client/src/components/Hotel_Item.jsx
+++ client/src/components/Hotel_Item.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Hotel() {
+function HotelItem() {
   return (
-    <div>Hotel</div>
+    <>
+    <li className="hotels_item"></li>
+    <Link className="hotels_item_link">
+      <figure className="hotels_item_pic-wrap">
+        <img src="" alt="Travel Image" 
+        className="hotels_item_img" />
+
+      </figure>
+
+    </Link>
+    </>
   )
 }
+
+
+export default HotelItem
